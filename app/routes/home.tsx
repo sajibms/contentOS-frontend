f3d70522574021d56610b2db5c7ac1d20eee49e4
sajibms <shahadath.sajib.ms@gmail.com>
2025-04-23 08:22:44 +0600

    feat: added text editor in to drawer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import TextEditor from "~/components/Editor/textEditor/TextEditor";
 import type { Route } from "./+types/home";
+import { Drawer, DrawerContent, DrawerTrigger } from "~/components/ui/drawer";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +13,16 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Welcome to React Router!</h1>
-      <TextEditor />
+      {/* <TextEditor /> */}
+
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent>
+          <div className="flex h-full w-full items-center justify-center">
+            <TextEditor />
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
